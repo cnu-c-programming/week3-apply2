5203b50633eb2ee493338b4c083a1f16279a6c95
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-20 12:03:28 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main() {
+void swap(int *x, int *y) {
+    int a = *x;
+    int b = *y;
+    *x = b;
+    *y = a;
+}
+
+int main(void) {
+    int x = 100, y = 200;
+
+    printf("x = %d, y = %d\n", x, y);
+    swap(&x,&y);
+    printf("x = %d, y = %d\n", x, y);
 
     return 0;
 }
