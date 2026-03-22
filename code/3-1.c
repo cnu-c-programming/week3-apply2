5bafb0723681b09e5639747b5691fe111deb3c1b
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-22 22:21:26 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int call_count() {
+    int static count = 0;
+    return ++count;
+}
+
+int main(void) {
+
+   printf("%d\n", call_count());
+   printf("%d\n", call_count());
+   printf("%d\n", call_count());
 
     return 0;
 }
