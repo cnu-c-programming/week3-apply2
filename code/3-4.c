6f79b64b95e6230382ac962354f844a8a43e0da2
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-23 00:22:23 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+int max_of(int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+    int result = va_arg(ap, int);
 
-    return 0;
+   for (int i = 0; i< count-1; i++)
+    {
+    int current = va_arg(ap, int);
+    if (current > result)
+    {
+        result = current;
+    }
 }
+    va_end(ap);
+    return result;
+}
+
+
+int main() {
+    printf("%d\n", max_of(4, 5, 12, 3, 9));
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    printf("%d\n", max_of(3, 100, 200, 300));
+    return 0;
+} 
