567c710b0b6a95e4bba0423ed18083412d1f3eb3
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-22 23:06:04 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+int average(int count, ...) {
+    va_list av;
+    va_start(av, count);
+    int total = 0;
+    for (int i = 0; i < count; i++) {
+        total += va_arg(av, int);
+    }
+    va_end(av);
+    return total/count;
+}
 
 int main() {
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
 
     return 0;
-}
+} 
