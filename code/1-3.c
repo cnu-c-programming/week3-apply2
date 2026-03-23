faa075a9ef2a6fd4716648660d4911c5bfc67433
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-23 14:02:09 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int power(int base, int exp) {
+    if (exp == 0)
+    {
+        return 1;
+    }
+    exp = exp -1;
+    return base*power(base, exp);
+}
 
+int main() {
+    printf("%d\n", power(2,3));
+    printf("%d\n", power(5,0));
+    printf("%d\n", power(3,4));
     return 0;
 }
