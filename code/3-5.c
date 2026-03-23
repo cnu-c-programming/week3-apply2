bba54c4e211a5a114ad0f38d7493779310ac459c
apply2 <67138665+apply2@users.noreply.github.com>
2026-03-23 15:17:06 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void my_sum (char type, int cnt, ...) {
+    va_list ap;
+    va_start(ap, cnt);
+
+    if (type == 'D')
+    {
+        int result = 0;
+        for (int i = 0; i < cnt; i++)
+        {
+            result += va_arg(ap,int);
+        }
+        printf("%d\n", result);
+    }
+    else if (type == 'C')
+    {
+        for (int i = 0; i < cnt; i++)
+        {
+            printf("%c", (char)va_arg(ap, int));
+        }
+        printf("\n");
+    }
+    else if (type == 'S')
+    {
+        for (int i = 0; i < cnt; i++) 
+        {
+            printf("%s%s", va_arg(ap, char*), (i < cnt - 1) ? " " : "");
+        }
+        printf("\n");
+    }
+    va_end(ap);
+}
+
 int main() {
+
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C','N','U');
+    my_sum('D', 4, 10, 20, 30, 40);
 
     return 0;
 }
